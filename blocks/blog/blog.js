a300492680b070a51fddf7aa0eb20472a10c3b96
alaintresor <tresoralain35@gmail.com>
2024-03-27 12:20:16 +0200

    fix string issue

--- blocks/blog/blog.js
+++ blocks/blog/blog.js
@@ -1,4 +1,4 @@
-import { createOptimizedPicture } from "../../scripts/aem.js";
+import { createOptimizedPicture } from '../../scripts/aem.js';
 
 async function fetchData(jsonURL) {
   const response = await fetch(jsonURL);
@@ -10,19 +10,19 @@ async function createListItems(jsonURL) {
   const { pathname } = new URL(jsonURL);
   const data = await fetchData(pathname);
   const listItems = data.map((item) => {
-    const li = document.createElement("li");
-    const img = document.createElement("img");
+    const li = document.createElement('li');
+    const img = document.createElement('img');
     img.src = item.imageurl;
     img.alt = item.title;
-    const div = document.createElement("div");
-    div.className = "cards-card-body"; // Assuming this is the class for card body
-    const title = document.createElement("h3");
+    const div = document.createElement('div');
+    div.className = 'cards-card-body'; // Assuming this is the class for card body
+    const title = document.createElement('h3');
     title.textContent = item.title;
-    const description = document.createElement("p");
+    const description = document.createElement('p');
     description.textContent = item.description;
-    const link = document.createElement("a");
+    const link = document.createElement('a');
     link.href = item.articleurl;
-    link.textContent = "Read more";
+    link.textContent = 'Read more';
     div.append(title, description, link);
     li.append(img, div);
     return li;
@@ -33,18 +33,18 @@ async function createListItems(jsonURL) {
 export default async function decorate(block) {
   const jsonURL = block.querySelector('a[href$=".json"]'); // Assuming jsonUrl is defined as a data attribute in the block
 
-  const ul = document.createElement("ul");
+  const ul = document.createElement('ul');
   const listItems = await createListItems(jsonURL);
   listItems.forEach((li) => ul.appendChild(li));
 
-  ul.querySelectorAll("img").forEach((img) =>
+  ul.querySelectorAll('img').forEach((img) =>
     img
-      .closest("picture")
+      .closest('picture')
       .replaceWith(
-        createOptimizedPicture(img.src, img.alt, false, [{ width: "750" }])
+        createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])
       )
   );
 
-  block.textContent = "";
+  block.textContent = '';
   block.append(ul);
 }
